c0e65ab528b12773c2eaf5424bbf66b555690768
airlinesfarefinder <airlinesfarefinder@gmail.com>
2025-06-06 03:22:24 +0530

    update

--- inc/config.php
+++ inc/config.php
@@ -4,7 +4,7 @@
 function config($key)
 {
     static $configuration = [
-        'phone_number' => '(888) 919-2090',
+        'phone_number' => '(877) 694-3693',
         'email_id' => 'info@chasentravel.com',
         'address' => '1700 Quail Street, Suite 100 Newport Beach, CA 92660',
         'domain_name' => 'https://chasentravel.com',
