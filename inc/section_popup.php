1d61f3e6326657dbb150e00392270cdc281c00f4
airlinesfarefinder <airlinesfarefinder@gmail.com>
2025-06-07 03:05:16 +0530

    update

--- inc/section_popup.php
+++ inc/section_popup.php
@@ -3,8 +3,8 @@
     <div class="head">Travel Support Desk</div>
     <div class="banner" style="background-image:url('assets/img/travel_banner/banner-phone1.jpg')"></div>
     <div class="featured_lines">
-        <div class="line">Hot Deals on Airfare | Last-Minute Travel Offers</div>
-        <div class="line">Exclusive Phone-Only Discounts</div>
+        <div class="line">Reservations | Changes | Cancellations</div>
+        <div class="line">Flexibal Payments | 24/7 Support | Refunds</div>
     </div>
     <div class="cta">
         <span class="title">Instant Connection – Speak with an Expert Now</span>
@@ -13,12 +13,12 @@
             <button class="call_button">
                 <div class="icon animated_icon" style="background-image:url('assets/img/travel_banner/phone.gif')"></div>
                 <div class="text">
-                    <h3>limited-time call-only specials</h3>
+                    <h3>24/7 Customer Support</h3>
                     <p><?php echo config('phone_number'); ?></p>
                 </div>
             </button>
         </div>
-        <p class="outrow">Connect with Travel Experts & Unlock Up to 45% Off on Top Destinations!</p>
+        <p class="outrow">Connect with Travel Experts & Unlock Best deals on Top Destinations!</p>
     </div>
     <ul class="pills">
         <li>All-Inclusive Holiday Deals</li>
@@ -34,7 +34,7 @@
         <button class="call_button">
             <div class="icon animated_icon" style="background-image:url('assets/img/travel_banner/call-ezgif.com-resize.gif');background-size:120%"></div>
             <div class="text">
-                <h3>Enjoy Up to <span>45% Off</span> on Holiday Offers!</h3>
+                <h3>24/7 Customer Support</h3>
                 <p><?php echo config('phone_number'); ?></p>
             </div>
         </button>
