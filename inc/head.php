d16820d9683071e7ed3e6fab98c258e09cb95983
airlinesfarefinder <airlinesfarefinder@gmail.com>
2025-07-17 21:58:57 +0530

    Update head

--- inc/head.php
+++ inc/head.php
@@ -19,12 +19,12 @@
 
 
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+<!--     <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/my-style.css">
 <link rel="stylesheet" href="css/deesha-love.css">
 <link rel="stylesheet" href="css/animate.css">
 <link rel="stylesheet" href="css/owl.carousel.min.css">
-<link rel="stylesheet" href="css/owl.theme.min.css">
+<link rel="stylesheet" href="css/owl.theme.min.css"> -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <link rel="stylesheet" href="assets/css/travel_banner.css">
