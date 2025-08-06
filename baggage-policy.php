b1c7a1d9c5d70fe8200ec371290246b0ecd89e2e
airlinesfarefinder <airlinesfarefinder@gmail.com>
2025-08-06 19:17:09 +0530

    Update

--- baggage-policy.php
+++ baggage-policy.php
@@ -11,6 +11,12 @@ include 'inc/head.php';
 
 
 <body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TD467JG4"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
     <?php
     include 'inc/nav.php';
     include 'inc/policies/baggage.php';
--- contact.php
+++ contact.php
@@ -10,6 +10,12 @@ include 'inc/head.php';
 <link rel="stylesheet" href="assets/css/contact.css">
 
 <body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TD467JG4"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
     <?php
     include 'inc/nav.php';
     include 'inc/contact/small_banner.php';
