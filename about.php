52c8d13cec42197ed379b5d76ef61d96d7085c6f
airlinesfarefinder <airlinesfarefinder@gmail.com>
2025-08-11 22:56:16 +0530

    Update

--- about.php
+++ about.php
@@ -8,13 +8,7 @@ include 'inc/head.php';
 ?>
 
 <body>
-
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TD467JG4"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-
-    <?php
+<?php
     include 'inc/nav.php';
     include 'inc/about/small_banner.php';
     include 'inc/about/about.php';
