3b60124bd2aa7debd72f551655b5a9b06f61fa07
airlinesfarefinder <airlinesfarefinder@gmail.com>
2025-08-11 22:01:10 +0530

    Update

--- inc/head.php
+++ inc/head.php
@@ -7,10 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
 
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self'">
-<meta http-equiv="X-Content-Type-Options" content="nosniff">
-<meta http-equiv="X-Frame-Options" content="DENY">
-
     <title>ChaseNTravel</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
